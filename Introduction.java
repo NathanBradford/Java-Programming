4f72aa93e9322300776eef44337a46b8faa2f369
Nathan Bradford <34286794+NathanBradford@users.noreply.github.com>
2017-12-10 21:09:05 -0800

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction {
+    public static void main(String []args) {
+        System.out.println("My Name is Nathan Bradford");
+        System.out.println("My Favorite Color is Green");
+        System.out.println("If I won the lottery I would Save for College");               
+    }
+}
